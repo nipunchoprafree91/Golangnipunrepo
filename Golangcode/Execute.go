4990252596d3ac10ccc6fc91c22f11fa5dbbcaaf
nipunchoprafree91 <91chopranipunfree@gmail.com>
2022-04-16 18:26:48 +0530

    add test function

--- Golangcode/Execute.go
+++ Golangcode/Execute.go
@@ -6,8 +6,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/nipunchoprafree91/Golangcode/Mytestinglib"
 )
 
 func main() {
 	fmt.Println("Starting Execution of the main function")
+	Mytestinglibfunc()
 }
