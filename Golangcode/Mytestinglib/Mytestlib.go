75b03e969e0b609f810444a19c164ce713026171
nipunchoprafree91 <91chopranipunfree@gmail.com>
2022-04-17 16:36:43 +0530

    New changes with goroutines

--- Golangcode/Mytestinglib/Mytestlib.go
+++ Golangcode/Mytestinglib/Mytestlib.go
@@ -3,13 +3,17 @@ package Mytestinglib
 import (
 	"fmt"
 	"github.com/nipunchoprafree91/Golangcode/Mychannel"
+	"time"
 )
 
 func Mytestinglibfunc() {
 	fmt.Println("Starting Execute of the Mytesting lib function")
 	fmt.Println("Calling My channel Code From Testing library")
 	go Mychannel.Mychannelfunc()
+	time.Sleep(1 * time.Second)
 	go Mychannel.Mychannelfunc1()
+	time.Sleep(1 * time.Second)
 	go Mychannel.Mychannelfunc2()
+	time.Sleep(1 * time.Second)
 
 }
